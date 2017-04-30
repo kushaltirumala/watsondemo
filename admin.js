0de3cdd4ac671de82940ed3321ea312a32c81c68
kushaltirumala <kushaltirumala99@gmail.com>
2017-04-30 15:49:17 -0700

    fixed it

--- admin.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	sstusername:"11b79bad-6256-40b5-87b6-f6dc26be9a4d",
-	sstpassword: "NsH8ioXMxLbU",
-	ttsusername: "8008ce76-6c87-4965-9594-1aca084d8bf1",
-	ttspassword: "cV7xH5hsI8YP",
-	translateusername:"51a3cbe3-c9eb-48ed-9305-583309808167",
-	translatepassword:"13iBjrLHGm3Z"
-
-}
